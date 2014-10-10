f914073c0e4edfcac53fa98b09c475c6ae2534fa
rinzaimedia <rinzaimedia@gmail.com>
2014-10-09 22:41:04 -0700

    changes to sites page

--- application/controllers/page.php
+++ application/controllers/page.php
@@ -19,6 +19,25 @@ class Page extends CI_Controller {
      */
     public function index($page = false)
     {
+        $this->load->model(array('page_model','sites_model', 'sales_model'));
+
+        $this->load->helper('url');
+
+        //$query = $this->db->query("select * from settings where id = '1'");
+
+        $results['results'] = $this->sites_model->getSettings();
+
+        //$results['pages'] = $this->page_model->getPages();
+
+        $results['sales'] = $this->sales_model->getSalesContent();
+
+        $results['links'] = $this->sites_model->getLinks();
+
+        $results['home'] = $this->sites_model->getHomeContent();
+
+        $results['rss'] = json_decode($this->sites_model->getRSS("http://feeds.feedburner.com/dailyrealestatenews"), TRUE);
+
+        $results['weather'] = json_decode($this->sites_model->getWeather($results['results'][0]['city'], $results['results'][0]['statecountry']), TRUE);
 
         if($page != false)
         {
