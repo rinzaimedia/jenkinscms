42b6de4ad79e67e6a731076c46012ee484e5d3e2
rinzaimedia <rinzaimedia@gmail.com>
2014-10-28 23:19:41 -0700

    changes to ajax

--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -1,8 +1,10 @@
 <!-- JavaScript -->
 
 <!-- Page Specific Plugins -->
+<div class="col-lg-12">
 <footer>
     &copy; <?php echo date('Y'); ?> Ironhead Services LLC | JenkinsCMS
 </footer>
+</div>
 </body>
 </html>
