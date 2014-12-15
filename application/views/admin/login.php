fa3f204724a968b25243a67c04cd04ae99aa9074
rinzaimedia <rinzaimedia@gmail.com>
2014-12-14 20:59:00 -0800

    for login system

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -32,6 +32,8 @@
 <body>
 <div id="page-wrapper">
     <div class="row">
+        <div id="error"></div>
+        <div id="result"></div>
         <div class="col-md-6">
             <form method="post" id="loginform">
                 <div class="form-group">
