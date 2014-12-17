8f21d6bf2b3b951473c62843892776108be7bd42
rinzaimedia <rinzaimedia@gmail.com>
2014-12-16 19:14:05 -0800

    for login system

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,6 +1,6 @@
 <?php if($results[0]['showscroller'] == 1):?>
 
-    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel" style="min-height: 300px; max-width: 768px;">
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel" style="min-height: 300px;">
         <!-- Indicators -->
         <ol class="carousel-indicators">
             <?php $count = 0;?>
@@ -21,7 +21,7 @@
                     </div>
 
                     <?php if($sales[$i]['salesimage'] != ''):?>
-                        <img src="<?php echo $sales[$i]['salesimage'];?>" class="img-responsive" alt="" style="max-height: 300px;" />
+                        <img src="<?php echo $sales[$i]['salesimage'];?>" class="img-responsive center-block" alt="" style="max-height: 300px;" />
 
                     <?php endif; ?>
                 </div>
