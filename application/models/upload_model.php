abb5fa1848a64ce52202b3f1b854824499e13d65
rinzaimedia <rinzaimedia@gmail.com>
2014-12-23 22:14:12 -0800

    for timezones

--- application/models/upload_model.php
+++ application/models/upload_model.php
@@ -1,5 +1,29 @@
 <?php class Upload_model extends CI_Model{
 
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->helper(array('form', 'url'));
+    }
 
+    function do_upload()
+    {
+        $config['upload_path'] = './uploads/';
+        $config['allowed_types'] = 'gif|jpg|png';
 
+        $this->load->library('upload', $config);
+
+        if ( ! $this->upload->do_upload())
+        {
+            $error = array('error' => $this->upload->display_errors());
+
+            $this->load->view('admin/upload_form', $error);
+        }
+        else
+        {
+            $data = array('upload_data' => $this->upload->data());
+
+            $this->load->view('admin/upload_success', $data);
+        }
+    }
 }
