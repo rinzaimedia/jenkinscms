f80a775d6cab0d93b472be7f59f16446593b1864
rinzaimedia <rinzaimedia@gmail.com>
2014-12-23 20:29:08 -0800

    for timezones

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -2,21 +2,6 @@
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
 
     public function __construct()
     {
@@ -24,11 +9,12 @@ class Welcome extends CI_Controller {
     }
 	public function index()
 	{
-        
+        $this->load->model('page_model');
+        $this->load->model('sites_model');
+        $this->load->model('sales_model');
+        $this->load->model('work_model');
 
         $this->load->helper('url');
-
-        //$query = $this->db->query("select * from settings where id = '1'");
 
         $results['results'] = $this->sites_model->getSettings();
 
