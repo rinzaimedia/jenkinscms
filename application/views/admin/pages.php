9187b7927d24942b5730db72573f671ebc6f6c7c
rinzaimedia <rinzaimedia@gmail.com>
2014-12-24 20:04:56 -0800

    table view on pages

--- application/views/admin/pages.php
+++ application/views/admin/pages.php
@@ -20,7 +20,10 @@
                     <div class="col-md-10"><a href="/manage/pages/<?php echo $page['pageid'];?>"><li class="fa fa-pencil"> <?php echo $page['pagetitle'];?></li></a></div>
                 </div>
                 <?php endforeach; ?>
-                <table data-toggle="table" data-url="/ajax/getpages" data-cache="false" data-height="299">
+                <div class="alert alert-success" id="events-result" data-es="Aquí se muestra el resultado del evento">
+                    Here is the result of event.
+                </div>
+                <table id="events-table" data-toggle="table" data-url="/ajax/getpages" data-cache="false" data-height="299" data-pagination="true" data-search="true">
                     <thead>
                     <tr>
                         <th data-field="pageid">Page ID</th>
@@ -36,3 +39,83 @@
     </div>
 </div>
 <script type="text/javascript" src="/assets/custom/js/bootstrap.table.js"></script>
+<script>
+    $(function () {
+        $('#basic-events-table').next().click(function () {
+            $(this).hide();
+
+            var $result = $('#events-result');
+
+            $('#events-table').bootstrapTable({
+                /*
+                 onAll: function (name, args) {
+                 console.log('Event: onAll, data: ', args);
+                 }
+                 onClickRow: function (row) {
+                 $result.text('Event: onClickRow, data: ' + JSON.stringify(row));
+                 },
+                 onDblClickRow: function (row) {
+                 $result.text('Event: onDblClickRow, data: ' + JSON.stringify(row));
+                 },
+                 onSort: function (name, order) {
+                 $result.text('Event: onSort, data: ' + name + ', ' + order);
+                 },
+                 onCheck: function (row) {
+                 $result.text('Event: onCheck, data: ' + JSON.stringify(row));
+                 },
+                 onUncheck: function (row) {
+                 $result.text('Event: onUncheck, data: ' + JSON.stringify(row));
+                 },
+                 onCheckAll: function () {
+                 $result.text('Event: onCheckAll');
+                 },
+                 onUncheckAll: function () {
+                 $result.text('Event: onUncheckAll');
+                 },
+                 onLoadSuccess: function (data) {
+                 $result.text('Event: onLoadSuccess, data: ' + data);
+                 },
+                 onLoadError: function (status) {
+                 $result.text('Event: onLoadError, data: ' + status);
+                 },
+                 onColumnSwitch: function (field, checked) {
+                 $result.text('Event: onSort, data: ' + field + ', ' + checked);
+                 },
+                 onPageChange: function (number, size) {
+                 $result.text('Event: onPageChange, data: ' + number + ', ' + size);
+                 },
+                 onSearch: function (text) {
+                 $result.text('Event: onSearch, data: ' + text);
+                 }
+                 */
+            }).on('all.bs.table', function (e, name, args) {
+                console.log('Event:', name, ', data:', args);
+            }).on('click-row.bs.table', function (e, row, $element) {
+                $result.text('Event: click-row.bs.table, data: ' + JSON.stringify(row));
+            }).on('dbl-click-row.bs.table', function (e, row, $element) {
+                $result.text('Event: dbl-click-row.bs.table, data: ' + JSON.stringify(row));
+            }).on('sort.bs.table', function (e, name, order) {
+                $result.text('Event: sort.bs.table, data: ' + name + ', ' + order);
+            }).on('check.bs.table', function (e, row) {
+                $result.text('Event: check.bs.table, data: ' + JSON.stringify(row));
+            }).on('uncheck.bs.table', function (e, row) {
+                $result.text('Event: uncheck.bs.table, data: ' + JSON.stringify(row));
+            }).on('check-all.bs.table', function (e) {
+                $result.text('Event: check-all.bs.table');
+            }).on('uncheck-all.bs.table', function (e) {
+                $result.text('Event: uncheck-all.bs.table');
+            }).on('load-success.bs.table', function (e, data) {
+                $result.text('Event: load-success.bs.table');
+            }).on('load-error.bs.table', function (e, status) {
+                $result.text('Event: load-error.bs.table, data: ' + status);
+            }).on('column-switch.bs.table', function (e, field, checked) {
+                $result.text('Event: column-switch.bs.table, data: ' +
+                    field + ', ' + checked);
+            }).on('page-change.bs.table', function (e, size, number) {
+                $result.text('Event: page-change.bs.table, data: ' + number + ', ' + size);
+            }).on('search.bs.table', function (e, text) {
+                $result.text('Event: search.bs.table, data: ' + text);
+            });
+        });
+    });
+</script>
