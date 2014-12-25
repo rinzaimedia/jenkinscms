75c1517354796c6ef76251d1861ea4c7cc8bbbf0
rinzaimedia <rinzaimedia@gmail.com>
2014-12-24 22:36:53 -0800

    table view on work items

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -87,40 +87,31 @@
                 <?php endif; ?>
             </div>
             <div class="col-sm-3 col-xs-3 text-center"><a href="mailto:<?php echo $results[0]['email'];?>"><i class="fa fa-envelope-o fa-2x"></i></a></div>
-            <nav class="navbar navbar-default">
-                <div class="container-fluid">
-                    <!-- Brand and toggle get grouped for better mobile display -->
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand" href="#">Brand</a>
-                    </div>
+            <div class="col-sm-3 col-xs-3 ">
+                <nav class="navbar navbar-default">
+                    <div class="container-fluid">
+                        <!-- Brand and toggle get grouped for better mobile display -->
 
-                    <!-- Collect the nav links, forms, and other content for toggling -->
-                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
-                        <ul class="nav navbar-nav navbar-right">
-                            <li><a href="#">Link</a></li>
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                                <ul class="dropdown-menu" role="menu">
-                                    <?php for($x = 0; $x < count($pages); $x++):?>
+                        <!-- Collect the nav links, forms, and other content for toggling -->
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
-                                        <?php if($pages[$x]['visible'] == '1'):?>
-                                            <li><a href="/static/<?php echo $pages[$x]['pageurl'];?>"><?php echo $pages[$x]['pagetitle'];?></a></li>
-                                        <?php endif; ?>
+                            <ul class="nav navbar-nav navbar-right">
 
-                                    <?php endfor; ?>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div><!-- /.navbar-collapse -->
-                </div><!-- /.container-fluid -->
-            </nav>
+                                        <?php for($x = 0; $x < count($pages); $x++):?>
+
+                                            <?php if($pages[$x]['visible'] == '1'):?>
+                                                <li><a href="/static/<?php echo $pages[$x]['pageurl'];?>"><?php echo $pages[$x]['pagetitle'];?></a></li>
+                                            <?php endif; ?>
+
+                                        <?php endfor; ?>
+
+                                </li>
+                            </ul>
+                        </div><!-- /.navbar-collapse -->
+                    </div><!-- /.container-fluid -->
+                </nav>
+            </div>
         </div>
     </div>
 </div>
