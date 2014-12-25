fa748002cb0e0e2647def46326f5c42515cf4572
rinzaimedia <rinzaimedia@gmail.com>
2014-12-24 20:14:07 -0800

    table view on pages

--- application/views/admin/pages.php
+++ application/views/admin/pages.php
@@ -23,15 +23,14 @@
                 <div class="alert alert-success" id="events-result" data-es="Aquí se muestra el resultado del evento">
                     Here is the result of event.
                 </div>
-                <table id="events-table"  data-url="/ajax/getpages" data-cache="false" data-height="299"
+                <table id="events-table" data-toggle="table" data-url="/ajax/getpages" data-cache="false" data-height="299"
                        data-pagination="true" data-search="true" data-show-columns="true">
                     <thead>
                     <tr>
-                        <th data-field="state" data-checkbox="true"></th>
                         <th data-field="pageid" data-sortable="true">Page ID</th>
                         <th data-field="pageurl" data-sortable="true">URL</th>
                         <th data-field="pagetitle" data-sortable="true">Title</th>
-
+                        <th data-field="operate" data-formatter="operateFormatter" data-events="operateEvents">Options</th>
                     </tr>
                     </thead>
                 </table>
@@ -42,82 +41,27 @@
 </div>
 <script type="text/javascript" src="/assets/custom/js/bootstrap.table.js"></script>
 <script>
-    $(function () {
-        $('#basic-events-table').next().click(function () {
-            $(this).hide();
+    function operateFormatter(value, row, index) {
+        return [
 
-            var $result = $('#events-result');
+            '<a class="edit ml10" href="javascript:void(0)" title="Edit">',
+            '<i class="fa fa-edit"></i>',
+            '</a>',
+            '<a class="remove ml10" href="javascript:void(0)" title="Remove">',
+            '<i class="fa fa-times-circle-o"></i>',
+            '</a>'
+        ].join('');
+    }
 
-            $('#events-table').bootstrapTable({
-                /*
-                 onAll: function (name, args) {
-                 console.log('Event: onAll, data: ', args);
-                 }
-                 onClickRow: function (row) {
-                 $result.text('Event: onClickRow, data: ' + JSON.stringify(row));
-                 },
-                 onDblClickRow: function (row) {
-                 $result.text('Event: onDblClickRow, data: ' + JSON.stringify(row));
-                 },
-                 onSort: function (name, order) {
-                 $result.text('Event: onSort, data: ' + name + ', ' + order);
-                 },
-                 onCheck: function (row) {
-                 $result.text('Event: onCheck, data: ' + JSON.stringify(row));
-                 },
-                 onUncheck: function (row) {
-                 $result.text('Event: onUncheck, data: ' + JSON.stringify(row));
-                 },
-                 onCheckAll: function () {
-                 $result.text('Event: onCheckAll');
-                 },
-                 onUncheckAll: function () {
-                 $result.text('Event: onUncheckAll');
-                 },
-                 onLoadSuccess: function (data) {
-                 $result.text('Event: onLoadSuccess, data: ' + data);
-                 },
-                 onLoadError: function (status) {
-                 $result.text('Event: onLoadError, data: ' + status);
-                 },
-                 onColumnSwitch: function (field, checked) {
-                 $result.text('Event: onSort, data: ' + field + ', ' + checked);
-                 },
-                 onPageChange: function (number, size) {
-                 $result.text('Event: onPageChange, data: ' + number + ', ' + size);
-                 },
-                 onSearch: function (text) {
-                 $result.text('Event: onSearch, data: ' + text);
-                 }
-                 */
-            }).on('all.bs.table', function (e, name, args) {
-                console.log('Event:', name, ', data:', args);
-            }).on('click-row.bs.table', function (e, row, $element) {
-                $result.text('Event: click-row.bs.table, data: ' + JSON.stringify(row));
-            }).on('dbl-click-row.bs.table', function (e, row, $element) {
-                $result.text('Event: dbl-click-row.bs.table, data: ' + JSON.stringify(row));
-            }).on('sort.bs.table', function (e, name, order) {
-                $result.text('Event: sort.bs.table, data: ' + name + ', ' + order);
-            }).on('check.bs.table', function (e, row) {
-                $result.text('Event: check.bs.table, data: ' + JSON.stringify(row));
-            }).on('uncheck.bs.table', function (e, row) {
-                $result.text('Event: uncheck.bs.table, data: ' + JSON.stringify(row));
-            }).on('check-all.bs.table', function (e) {
-                $result.text('Event: check-all.bs.table');
-            }).on('uncheck-all.bs.table', function (e) {
-                $result.text('Event: uncheck-all.bs.table');
-            }).on('load-success.bs.table', function (e, data) {
-                $result.text('Event: load-success.bs.table');
-            }).on('load-error.bs.table', function (e, status) {
-                $result.text('Event: load-error.bs.table, data: ' + status);
-            }).on('column-switch.bs.table', function (e, field, checked) {
-                $result.text('Event: column-switch.bs.table, data: ' +
-                    field + ', ' + checked);
-            }).on('page-change.bs.table', function (e, size, number) {
-                $result.text('Event: page-change.bs.table, data: ' + number + ', ' + size);
-            }).on('search.bs.table', function (e, text) {
-                $result.text('Event: search.bs.table, data: ' + text);
-            });
-        });
-    });
+    window.operateEvents = {
+        
+        'click .edit': function (e, value, row, index) {
+            alert('You click edit icon, row: ' + JSON.stringify(row));
+            console.log(value, row, index);
+        },
+        'click .remove': function (e, value, row, index) {
+            alert('You click remove icon, row: ' + JSON.stringify(row));
+            console.log(value, row, index);
+        }
+    };
 </script>
